a6b06cde4d6fa60ceecc88ed8b748a0201b16bc6
nafisanarzieva <nafisanarzieva@nafisas-mbp>
2020-12-29 10:10:37 -0500

    A new repository has been created

---
+++ src/test/java/com/automation/test/DataProviderTest.java
@@ -0,0 +1,21 @@
+package com.automation.test;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.automation.helper.TestBase;
+
+public class DataProviderTest extends TestBase {
+ 
+    @DataProvider(name = "dataMethod")
+    public static Object[][] dataMethod() {
+    	System.out.println("Data Provider Method");
+        return new Object[][] { { 0 }, { 1 } };
+    }
+
+	@Test(priority = 1)
+	public void FBLogin() throws Exception {
+		System.out.println("Test Method");
+	}
+
+}
